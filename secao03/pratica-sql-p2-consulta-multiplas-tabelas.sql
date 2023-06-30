93ae69f6f6877e621a80b57f4b2fac72c8276431
Ariel Abade <arielabadebandeira@gmail.com>
2023-06-30 15:46:22 -0300

    30/06/2023

--- secao03/pratica-sql-p2-consulta-multiplas-tabelas.sql
+++ secao03/pratica-sql-p2-consulta-multiplas-tabelas.sql
@@ -5,6 +5,8 @@ USE secao05;
 SELECT * FROM tipos_produto;
 SELECT * FROM produtos;
 
+-- a pilha de execução no SQL também é importante, primeiro precisa dar o SELECT, e depois fazer o resto
+
 -- Conulsta em multiplas tabelas
 SELECT p.codigo AS 'Codigo', p.descricao AS 'Descricao', p.preco AS 'Preco', tp.descricao AS 'Tipo Produto' 
 	FROM produtos AS p, tipos_produto AS tp
